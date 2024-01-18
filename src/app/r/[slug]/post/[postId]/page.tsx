05678e85715fdbca8e27893c8da73c66e207237b
lucsa <seyadeodin@gmail.com>
2024-01-18 16:39:14 -0300

    Create detail page

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,4 +1,14 @@
-import { FC } from 'react';
+import PostVoteServer from '@/components/post-vote/PostVoteServer';
+import { buttonVariants } from '@/components/ui/Button';
+import EditorOutput from '@/components/ui/EditorOutput';
+import { db } from '@/lib/db';
+import { redis } from '@/lib/redis';
+import { formatTimeToNow } from '@/lib/utils';
+import { CachedPost } from '@/types/redits';
+import { Post, User, Vote } from '@prisma/client';
+import { ArrowBigDown, ArrowBigUp, Loader2 } from 'lucide-react';
+import { notFound } from 'next/navigation';
+import { FC, Suspense } from 'react';
 
 interface PageProps {
   params: {
@@ -9,8 +19,80 @@ interface PageProps {
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
-const page = ({ params }: PageProps) => {
-  return <div>Post</div>;
+const page = async ({ params }: PageProps) => {
+  const cachedPost = (await redis.hgetall(
+    `post:${params.postId}`
+  )) as CachedPost;
+
+  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+
+  if (!cachedPost) {
+    post = await db.post.findFirst({
+      where: {
+        id: params.postId,
+      },
+      include: {
+        votes: true,
+        author: true,
+      },
+    });
+  }
+
+  if (!post && !cachedPost) return notFound();
+
+  return (
+    <div>
+      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+        <Suspense fallback={<PostVoteShell />}>
+          {/* @ts-expect-error server component */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
+        </Suspense>
+        <div className="sw:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          {/*truncate = text...*/}
+          <p className="max-h-40 mt-1 trucnate text-x text-gray-500">
+            Poisted by u/{post?.author.username ?? cachedPost.authorUserName}{' '}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
+
+          <EditorOutput content={post?.content ?? cachedPost.content} />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+function PostVoteShell() {
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      {/* upvote */}
+      <div className={buttonVariants({ variant: 'ghost' })}>
+        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      </div>
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h3 w-3 animate-spin" />
+      </div>
+      {/* downvote */}
+      <div className={buttonVariants({ variant: 'ghost' })}>
+        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      </div>
+    </div>
+  );
+}
 
 export default page;
---
+++ src/components/post-vote/PostVoteServer.tsx
@@ -0,0 +1,65 @@
+import { Post, Vote, VoteType } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+import { FC } from 'react';
+import PostVoteClient from './PostVoteClient';
+import { getAuthSession } from '@/lib/auth';
+
+interface PostVoteServerProps {
+  postId: string;
+  initialVotesAmt?: number;
+  initialVote?: VoteType | null;
+  getData?: () => Promise<(Post & { votes: Vote[] }) | null>;
+}
+
+const PostVoteServer = async ({
+  postId,
+  initialVotesAmt,
+  initialVote,
+  getData,
+}: PostVoteServerProps) => {
+  //const session = await getServerSession();
+  const session = await getAuthSession();
+
+  let _votesAmt = 0;
+  let _currentVote: VoteType | null | undefined = undefined;
+
+  if (getData) {
+    const post = await getData();
+    console.log(
+      'LS -> src/components/post-vote/PostVoteServer.tsx:26 -> post: ',
+      post
+    );
+    if (!post) return notFound();
+
+    _votesAmt = post.votes.reduce((acc, vote) => {
+      if (vote.type === 'UP') return acc + 1;
+      if (vote.type === 'DOWN') return acc - 1;
+      return acc;
+    }, 0);
+
+    _currentVote = post.votes.find((vote) => vote.userId === session?.user.id)
+      ?.type;
+    console.log(
+      'LS -> src/components/post-vote/PostVoteServer.tsx:40 -> session?.user: ',
+      session
+    );
+    console.log(
+      'LS -> src/components/post-vote/PostVoteServer.tsx:40 -> session?.user.id: ',
+      session?.user.id
+    );
+  } else {
+    _votesAmt = initialVotesAmt!;
+    _currentVote = initialVote;
+  }
+
+  return (
+    <PostVoteClient
+      postId={postId}
+      initialVotesAmt={_votesAmt}
+      initialVote={_currentVote}
+    />
+  );
+};
+
+export default PostVoteServer;
